9caf36166973ed85e20d027185e023ce070e6b67
tosery <83147935+tosery@users.noreply.github.com>
2021-10-31 12:23:49 +0900

    firebase

---
+++ app/src/main/java/com/example/hong_firebase/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hong_firebase;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.wrapper,new com.example.hong_firebase.recfragment()).commit();
+
+    }
+}
